75333baa5eb9510d03ad04404c2f1a855be8d827
Emanuele <emanuelestudent@LAPTOP-V6S8LTL2.localdomain>
2022-08-02 18:46:42 +0200

    Exercise-7 Retreive all resources

---
+++ Exercise-7-Retrieve-all-resources/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import 'express-async-errors';
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const app = express();
+
+app.get('/users', async (request,response) => {
+    const users = await prisma.user.findMany();
+
+    response.json(users)
+})
+
+export default app;
