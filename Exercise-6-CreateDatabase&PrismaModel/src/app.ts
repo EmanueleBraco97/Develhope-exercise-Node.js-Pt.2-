1e857993b639e20e0579591a6de029d157245b04
Emanuele <emanuelestudent@LAPTOP-V6S8LTL2.localdomain>
2022-08-03 15:49:25 +0200

    exercise-7 retreive all resources

--- Exercise-6-CreateDatabase&PrismaModel/src/app.ts
+++ Exercise-6-CreateDatabase&PrismaModel/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
 
+import prisma from "./lib/prisma/client";
+
 const app = express();
 
-app.get('/users', (request,response) => {
-    response.json({name: 'emanuele'})
+app.get('/users', async(request,response) => {
+    const users = await prisma.user.findMany();
+
+    response.json(users);
 })
 
 export default app;
--- Exercise-6-CreateDatabase&PrismaModel/src/server.test.ts
+++ Exercise-6-CreateDatabase&PrismaModel/src/server.test.ts
@@ -1,16 +1,31 @@
 import supertest from 'supertest';
 
+import {prismaMock} from "./lib/prisma/client.mock";
+
 import app from './app';
 
 const request = supertest(app);
 
+
 test('GET /users', async ()=> {
+    const users= [
+        {
+            id:1,
+            name:'emanuele',
+            surname:'braco',
+            role: 'student'
+        }
+    ]
+
+     //@ts-ignore
+    prismaMock.user.findMany.mockResolvedValue(users);
+
     const response = await request
         .get('/users')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 
-    expect(response.body).toEqual(
-        {name: 'emanuele'}
-    )
+    expect(response.body).toEqual(users);
 })
+
+
