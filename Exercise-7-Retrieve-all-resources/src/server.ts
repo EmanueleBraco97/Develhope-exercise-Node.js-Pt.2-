3d5ea0918c492039667776ed6b228366ad851734
Emanuele <emanuelestudent@LAPTOP-V6S8LTL2.localdomain>
2022-08-03 15:26:38 +0200

    delete exercise-7

--- Exercise-7-Retrieve-all-resources/src/server.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-import app from './app'
-
-process.env.PORT;
-
-app.listen(process.env.PORT, () => {
-    console.log(`[Server]: Server is running at http://localhost:${process.env.PORT}`)
-})
