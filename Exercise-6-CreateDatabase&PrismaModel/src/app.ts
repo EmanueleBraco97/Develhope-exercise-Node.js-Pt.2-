2766e5aa6834c21a4a709ec3b1510805103d7469
Emanuele <emanuelestudent@LAPTOP-V6S8LTL2.localdomain>
2022-08-25 16:21:03 +0200

    Exercise 14-Prepare your application for deployment

--- Exercise-6-CreateDatabase&PrismaModel/src/app.ts
+++ Exercise-6-CreateDatabase&PrismaModel/src/app.ts
@@ -5,13 +5,17 @@ import { ValidationErrorMiddleware } from './lib/middleware/validation';
 import { initCorsMiddleware } from './lib/middleware/cors';
 import { initSessionMiddleware } from './lib/middleware/session';
 import { passport } from "./lib/middleware/passport";
+import {
+    notFoundMiddleware,
+    initErrorMiddleware,
+} from './lib/middleware/error';
 
 import usersRoutes from './routes/users';
 import authRoutes from './routes/auth';
 
 const app = express();
 
-app.use(initSessionMiddleware());
+app.use(initSessionMiddleware(app.get('env')));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -21,7 +25,9 @@ app.use(initCorsMiddleware());
 
 app.use('/users', usersRoutes);
 app.use('/auth', authRoutes);
+app.use(notFoundMiddleware);
 
 app.use(ValidationErrorMiddleware);
+app.use(initErrorMiddleware(app.get('env')));
 
 export default app;
